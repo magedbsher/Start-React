0db5953175c9aadf4fa3394e6af9ae411f5de3cf
magedbsher <magedbsher@gmail.com>
2023-03-27 22:04:42 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Home from "./Component/Home/Home";
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/react-fontawesome";
+export default function App() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
